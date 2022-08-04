f9de6c81ed789fdffde28c1029503f9760a76e59
leedongyeop <ldy_1204@naver.com>
2022-08-04 12:53:41 +0900

    MQTT publisher가 임의 데이터를 주기적으로 보내는 코드

---
+++ publisher.js
@@ -0,0 +1,19 @@
+const mqtt = require('mqtt');
+
+const client = mqtt.connect('mqtt://broker.hivemq.com')
+
+const message = {
+    "sensor_name": "humudity",
+    "location": "N8",
+    "value": 80
+}
+
+setInterval(
+    () => {
+        message.value = (Math.floor(Math.random() * 35));
+
+        client.publish("green-campus/sensors", JSON.stringify(message));
+    },
+
+    2000
+);
